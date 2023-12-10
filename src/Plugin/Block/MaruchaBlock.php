049d4d27309d01b90cd4c7e56deca3e774c866fe
maruyan <7115497+hmaruyama@users.noreply.github.com>
2023-12-10 19:04:17 +0900

    プラグインタイプで、Annotationsの代わりにAttributesが利用できるように

--- src/Plugin/Block/MaruchaBlock.php
+++ src/Plugin/Block/MaruchaBlock.php
@@ -3,15 +3,16 @@
 namespace Drupal\marucha\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * Provides marucha block.
- *
- * @Block(
- *   id = "marucha_block",
- *   admin_label = @Translation("Marucha Block"),
- * )
  */
+#[Block(
+  id: 'marucha_block',
+  admin_label: new TranslatableMarkup('Marucha Block')
+)]
 class MaruchaBlock extends BlockBase {
 
   /**
